00f79b4775b36e32be79d879172769eeca915293
Greg P <gperlman27@gmail.com>
2017-12-23 16:43:12 -0500

    scaffold dialog content styling

--- src/containers/work/index.js
+++ src/containers/work/index.js
@@ -5,6 +5,9 @@ import { withStyles } from 'material-ui/styles';
 import Grid from 'material-ui/Grid';
 import Typography from 'material-ui/Typography/Typography';
 import Dialog from 'material-ui/Dialog';
+import Button from 'material-ui/Button';
+import ArrowBackIcon from 'material-ui-icons/ArrowBack';
+import Paper from 'material-ui/Paper';
 
 import { openDialog, closeDialog } from '../../modules/general';
 
@@ -13,6 +16,18 @@ import ProjectList from './ProjectList';
 const styles = theme => ({
     root: {
         marginTop: '25px',
+    },
+    button: {
+        position: 'fixed',
+        top: '25px',
+        left: '25px'
+    },
+    dialog: {
+        backgroundColor: 'blue',
+    },
+    dialogContent: {
+        marginTop: '100px',
+        padding: '25px',
     },
 });
 
@@ -36,8 +51,21 @@ const Work = props => {
                 open={props.dialog}
                 onClose={props.closeDialog}
             >
-                <button onClick={props.closeDialog}>close me</button>
-                {props.dialogHtml}
+                <Button fab mini onClick={props.closeDialog} color="primary" aria-label="cancel" className={classes.button}>
+                    <ArrowBackIcon />
+                </Button>
+                <Grid
+                    className={classes.dialog}
+                    container
+                    justify="center"
+                    alignContent="center"
+                >
+                    <Grid item xs={10}>
+                        <Paper className={classes.dialogContent}>
+                            {props.dialogHtml}
+                        </Paper>
+                    </Grid>
+                </Grid>
             </Dialog>
         </div >
     )
